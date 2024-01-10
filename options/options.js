dcc94608da94d4eadcc82c26cb66f83a64bfb3d0
Crowdbotics <engineering@crowdbotics.com>
2024-01-10 19:34:51 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "horoscopeapp-dev-132366",
+  url: "https://horoscopeapp-dev-132366.botics.co",
+  api: "https://horoscopeapp-dev-132366.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
